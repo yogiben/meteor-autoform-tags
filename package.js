a515f18bb53403d52071af8b79e0acbc58a56e8a
Michał Powaga <michalpowaga13@gmail.com>
2015-05-12 18:26:17 +0100

    Bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'yogiben:autoform-tags',
   summary: 'Tags input for aldeed:autoform',
-  version: '0.1.1',
+  version: '0.1.2',
   git: 'https://github.com/yogiben/meteor-autoform-tags'
 });
 
