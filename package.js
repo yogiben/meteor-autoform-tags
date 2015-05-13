f4f2c41a470a640ff60e3792c4fbdc689e6fcf3c
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-05-13 15:25:30 +0000

    bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'yogiben:autoform-tags',
   summary: 'Tags input for aldeed:autoform',
-  version: '0.1.2',
+  version: '0.1.3',
   git: 'https://github.com/yogiben/meteor-autoform-tags'
 });
 
